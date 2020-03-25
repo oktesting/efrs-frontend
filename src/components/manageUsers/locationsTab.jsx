f80aa986d6eeb80710d5008e0108146a79cc484a
trungtq23 <44627356+trungtq23@users.noreply.github.com>
2020-03-25 21:39:51 +0700

    add user locations tab

--- src/components/manageUsers/locationsTab.jsx
+++ src/components/manageUsers/locationsTab.jsx
@@ -1,9 +1,67 @@
 import React, { Component } from "react";
+import { getUserLocation } from "../../services/locationService";
 
 class LocationsTab extends Component {
-  state = {};
+  state = {
+    locations: []
+  };
+
+  renderlocationInfo(label, data) {
+    return (
+      <div className="form-group row">
+        <label className="col-4 col-form-label">{label}</label>
+        <div className="col-8">
+          <input
+            placeholder={label}
+            value={data}
+            className="form-control here"
+            type="text"
+            disabled
+          />
+        </div>
+      </div>
+    );
+  }
+
+  renderLocationCard(data) {
+    return (
+      <div>
+        <form>
+          <div>
+            {this.renderlocationInfo("Address", data.address)}
+            {this.renderlocationInfo("Latitude", data.lat)}
+            {this.renderlocationInfo("Longtitude", data.lng)}
+            {this.renderlocationInfo("Province", data.province)}
+            {this.renderlocationInfo("District", data.district)}
+          </div>
+        </form>
+      </div>
+    );
+  }
+
   render() {
-    return <div>Location</div>;
+    const { locations } = this.state;
+    return (
+      <div className="col shadow">
+        <div className="mt-3">
+          <h4>User Locations</h4>
+        </div>
+        <hr />
+        {locations != null && locations.length !== 0 ? (
+          <div>
+            {locations.map((item, i) => (
+              <div>
+                <h4>Location {i + 1}</h4>
+                {this.renderLocationCard(item)}
+                <hr />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <h6> There is no provided location </h6>
+        )}
+      </div>
+    );
   }
 }
 
